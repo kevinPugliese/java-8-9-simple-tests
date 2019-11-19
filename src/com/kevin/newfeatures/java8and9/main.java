4b026af750d3f7cccc0808cb8122866210993c36
Kevin Pugliese <kevin.lima.contato@gmail.com>
2019-11-18 22:37:23 -0300

    Main

---
+++ src/com/kevin/newfeatures/java8and9/main.java
@@ -0,0 +1,20 @@
+package com.kevin.newfeatures.java8and9;
+
+import com.kevin.newfeatures.java8and9.funcional.LambdaTest;
+import com.kevin.newfeatures.java8and9.lambda.Lambda;
+
+public class main {
+
+  public static void main(String[] args) {
+
+//    Lambda l = new Lambda();
+//    l.funcaoComLambdaExpression();
+//    l.funcaoSemLambda();
+
+    LambdaTest lambdaTest = new LambdaTest();
+    lambdaTest.testar();
+
+
+  }
+
+}
